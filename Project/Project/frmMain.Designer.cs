690535bec6165f4eb4e9b7c1c6aaac996b54a803
Alvin Lim Wan Sying <alvmbj@gmail.com>
2017-02-18 18:25:57 +0800

    Admi
    Admin

---
+++ Project/Project/frmMain.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Project
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.btnInventory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(12, 12);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(259, 236);
+            this.btnRecord.TabIndex = 0;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // btnInventory
+            // 
+            this.btnInventory.Location = new System.Drawing.Point(278, 12);
+            this.btnInventory.Name = "btnInventory";
+            this.btnInventory.Size = new System.Drawing.Size(259, 236);
+            this.btnInventory.TabIndex = 1;
+            this.btnInventory.Text = "Inventory";
+            this.btnInventory.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 257);
+            this.Controls.Add(this.btnInventory);
+            this.Controls.Add(this.btnRecord);
+            this.Name = "frmMain";
+            this.Text = "Xtronic Automation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnInventory;
+    }
+}
+
